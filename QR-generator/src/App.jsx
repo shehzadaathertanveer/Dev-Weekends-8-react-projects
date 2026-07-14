072a02c73f3959d3f335e7294c3dfd4cb53417cd
ather <shehzadaathertanveer@gmail.com>
2026-07-14 20:42:38 +0500

    added qr generator

--- QR-generator/src/App.jsx
+++ QR-generator/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
+import QRCode from 'react-qr-code' 
 
 function App() {
 
+  const [value,setValue]=useState('') 
+  const [qr,setQr] = useState('')
+
+  function generateCode(){
+    setQr(value)
+  }
+
   return (
-    <>
-      
-    </>
+    <div className='w-full h-screen bg-black flex justify-center items-center flex-col p-2 gap-3' >
+      <div className=' flex flex-row justify-between rounded-xl shadow-2xl bg-white'>
+        <input className=" p-4 text-black outline-none" type="text" placeholder='Enter value' onChange={(e)=>setValue(e.target.value)}/>
+        <button className='text-white bg-blue-700 p-4 ' onClick={generateCode}>Generate</button>
+      </div>
+      <div>
+        <QRCode value={qr}/>
+      </div>
+    </div>
   )
 }
 
